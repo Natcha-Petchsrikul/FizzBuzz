5984a10daf578e5c878ee0e8a06f0804b8ac33f6
Natcha Petchsrikul <natcha.p@arise.tech>
2024-05-15 10:57:05 +0700

    add Taxi func

--- testjs/src/taxi.test.ts
+++ testjs/src/taxi.test.ts
@@ -2,7 +2,27 @@ import {Taxi} from './taxi'
 
 describe('Taxi', () => {
   it('example test', () => {
-    const result = Taxi()
-    expect(result).toEqual(55)
+    const result = Taxi(1,1)
+    expect(result).toEqual(5)
+  })
+  it('example test2', () => {
+    const result = Taxi(1.1,1)
+    expect(result).toEqual(7)
+  })
+  it('example test3', () => {
+    const result = Taxi(1.6,1)
+    expect(result).toEqual(9)
+  })
+  it('example test4', () => {
+    const result = Taxi(1.6,1.2)
+    expect(result).toEqual(10)
+  })
+  it('example test4', () => {
+    const result = Taxi(1.6,1.8)
+    expect(result).toEqual(10)
+  })
+  it('example test4', () => {
+    const result = Taxi(1,1.8)
+    expect(result).toEqual(6)
   })
 })
--- testjs/src/taxi.ts
+++ testjs/src/taxi.ts
@@ -1,1 +1,8 @@
-export const Taxi = () => 55
+export const Taxi = (kilometer: number, minute: number) => {
+    const meter = kilometer * 100
+    const roundedKilometer = meter % 50 == 0 ? kilometer : (meter + (50 - (meter % 50))) / 100
+    const second = minute * 60
+    const roundedMinute = second % 60 == 0 ? minute : (second + (60 - (second % 60))) / 60
+    //const result = (4 * roundedKilometer) + roundedMinute
+    return  (4 * roundedKilometer) + roundedMinute
+}
