896dbf2ca8437a565ef462ec28289b1d453dfecb
Natcha Petchsrikul <natcha.p@arise.tech>
2024-05-16 09:19:25 +0700

    TestFizzBuzzShouldReturn1WhenInput1

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -1,5 +1,18 @@
 package main
 
-func FizzBuzz(n int) string {
-	return "1"
+import "strconv"
+
+type FizzBuzz struct {
+	input  int
+	result string
+}
+
+func NewFizzBuzz(input int) FizzBuzz {
+	fizzbuzz := FizzBuzz{}
+	fizzbuzz.input = input
+	fizzbuzz.result = ""
+	return fizzbuzz
+}
+func (f FizzBuzz) Check(n int) string {
+	return strconv.Itoa(f.input)
 }
--- testgo/fizzbuzz_test.go
+++ testgo/fizzbuzz_test.go
@@ -4,8 +4,9 @@ import "testing"
 
 func TestFizzBuzzShouldReturn1WhenInput1(t *testing.T) {
 	input := 1
+	fizzbuzz := NewFizzBuzz(input)
 
-	got := FizzBuzz(input)
+	got := fizzbuzz.Check(input)
 
 	want := "1"
 	if got != want {
