4e7eef9afe7a05d609d5fb3de772720b9322eea1
Natcha Petchsrikul <natcha.p@arise.tech>
2024-05-18 23:23:06 +0700

    TestFizzBuzzShouldReturnFizzBuzzWhenInput15

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -6,6 +6,7 @@ func FizzBuzz(n int) string {
 	result := strconv.Itoa(n)
 	result = check(result, "Fizz", n%3 == 0)
 	result = check(result, "Buzz", n%5 == 0)
+	result = check(result, "FizzBuzz", n%15 == 0)
 	return result
 }
 
