f96c931ab32453cc53bd59492abfd79d27085d8f
Natcha Petchsrikul <natcha.p@arise.tech>
2024-05-23 15:24:58 +0700

    TestFizzBuzzShouldReturnBuzzWhenInput5

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -5,10 +5,16 @@ import "strconv"
 func FizzBuzz(n int) string {
 	result := strconv.Itoa(n)
 	result = checkFizz(result)
+	result = checkBuzz(result)
 	return result
 }
 
 func checkFizz(s string) string {
 	n, _ := strconv.Atoi(s)
-	return (map[bool]string{true: "Fizz", false: s})[n%3 == 0]
+	return (map[bool]string{true: "Fizz", false: s})[n != 0 && n%3 == 0]
+}
+
+func checkBuzz(s string) string {
+	n, _ := strconv.Atoi(s)
+	return (map[bool]string{true: "Buzz", false: s})[n != 0 && n%5 == 0]
 }
