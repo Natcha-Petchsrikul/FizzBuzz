019ef52f197fec094d959236e2fe1fa46142db60
Natcha Petchsrikul <natcha.p@arise.tech>
2024-05-23 15:48:00 +0700

    TestFizzBuzzShouldReturnFizzWhenInput12

--- testgo/fizzbuzz_test.go
+++ testgo/fizzbuzz_test.go
@@ -142,3 +142,16 @@ func TestFizzBuzzShouldReturn11WhenInput11(t *testing.T) {
 		t.Errorf("got %q but want %q", got, want)
 	}
 }
+
+func TestFizzBuzzShouldReturnFizzWhenInput12(t *testing.T) {
+	input := 12
+
+	f := NewFizzBuzz(input)
+
+	got := f.Result
+
+	want := "Fizz"
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
+}
